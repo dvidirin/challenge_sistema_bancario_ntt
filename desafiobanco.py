a855214113db4e51f27edd05dcf65bf8c5a35400
dvidirin <dvn.face@gmail.com>
2024-08-28 17:54:16 -0300

    feat: adicionar funções criar usuário e criar conta corrente

--- desafiobanco.py
+++ desafiobanco.py
@@ -1,9 +1,39 @@
 def menu():
     print("\n--- Sistema Bancário ---")
-    print("1. Depositar")
-    print("2. Sacar")
-    print("3. Extrato")
-    print("4. Sair")
+    print("1. Criar Usuário")
+    print("2. Criar Conta Corrente")
+    print("3. Depositar")
+    print("4. Sacar")
+    print("5. Extrato")
+    print("6. Sair")
+
+def criar_usuario(usuarios):
+    cpf = input("Informe o CPF (somente números): ")
+    if any(usuario['cpf'] == cpf for usuario in usuarios):
+        print("Já existe um usuário com esse CPF!")
+        return usuarios
+    
+    nome = input("Informe o nome: ")
+    data_nascimento = input("Informe a data de nascimento (dd/mm/aaa): ")
+    endereco = input("Informe o endereço (logradouro, numero - bairro - cidade/sigla estado): ")
+
+    usuarios.append({"nome": nome, "data_nascimento":data_nascimento, "cpf":cpf, "endereco":endereco})
+    print("Usuário criado com sucesso!")
+    return usuarios
+
+def criar_conta_corrente(contas, usuarios):
+    cpf = input("Informe o CPF do usuário para vincular à conta: ")
+    usuario = next((usuario for usuario in usuarios if usuario['cpf'] == cpf), None)
+
+    if usuario:
+        numero_conta = len(contas) + 1
+        agencia = "0001"
+        contas.append({"agencia":agencia, "numero_conta":numero_conta, "usuario":usuario})
+        print(f"Conta Corrente criada com sucesso! Agência: {agencia} | Número da conta: {numero_conta}")
+    else:
+        print("Usuário não encontrado. Certifique-se de que o CPF está correto.")
+    
+    return contas
 
 def depositar(saldo, extrato):
     valor = float(input("Informe o valor para depósito: R$ "))
@@ -43,6 +73,8 @@ def exibir_extrato(saldo, extrato):
     print(f"\nSaldo atual: R$ {saldo:.2f}")
 
 def main():
+    usuarios = []
+    contas = []
     saldo = 0
     extrato = []
     saques_diarios = 0
@@ -52,16 +84,20 @@ def main():
         opcao = input("Escolha uma opção: ")
 
         if opcao == "1":
-            saldo, extrato = depositar(saldo, extrato)
+            usuarios = criar_usuario(usuarios)
         elif opcao == "2":
-            saldo, extrato, saques_diarios = sacar(saldo, extrato, saques_diarios)
+            contas = criar_conta_corrente(contas, usuarios)
         elif opcao == "3":
-            exibir_extrato(saldo, extrato)
+            saldo, extrato = depositar(saldo, extrato)
         elif opcao == "4":
+            saldo, extrato, saques_diarios = sacar(saldo, extrato, saques_diarios)
+        elif opcao == "5":
+            exibir_extrato(saldo, extrato)
+        elif opcao == "6":
             print("Saindo do sistema. Até logo!")
             break
         else:
             print("Opção inválida! Por favor, escolha uma opção válida.")
 
-if __name__=="__main__":
+if __name__ == "__main__":
     main()        
